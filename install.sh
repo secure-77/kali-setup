366be87410b487b1318809b3a9f0eb26a89144d4
sec77 <31564517+secure-77@users.noreply.github.com>
2026-05-08 10:47:36 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -37,7 +37,8 @@ cp .config/picom/picom.conf ~/.config/picom/picom.conf
 cp .config/rofi/config ~/.config/rofi/config
 cp .fehbg ~/.fehbg
 cp .config/i3/clipboard_fix.sh ~/.config/i3/clipboard_fix.sh
-cd ~/.config/i3
+sudo mkdir -p /usr/share/i3blocks/
+cd /usr/share/i3blocks/
 git clone https://github.com/vivien/i3blocks-contrib
 cd ~/kali-setup
 chmod +x ~/.config/i3/clipboard_fix.sh
